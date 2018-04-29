ac0837b45223c224b25c070e26f97a294b255a7d
Kamil Samigullin <kamil@samigullin.info>
2018-04-29 10:48:18 +0300

    issue #26: cover completion command

--- cmd/pkg_test.go
+++ cmd/pkg_test.go
@@ -1,1 +1,30 @@
-package cmd_test
+package cmd
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCompletion(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+	completionCmd.SetOutput(buf)
+	defer completionCmd.SetOutput(nil)
+	{
+		assert.Error(t, completionCmd.Args(completionCmd, nil))
+		assert.Error(t, completionCmd.Args(completionCmd, []string{"shell"}))
+		assert.NoError(t, completionCmd.Args(completionCmd, []string{bashFormat}))
+		assert.NoError(t, completionCmd.Args(completionCmd, []string{zshFormat}))
+	}
+	{
+		buf.Reset()
+		assert.NoError(t, completionCmd.RunE(completionCmd, []string{bashFormat}))
+		assert.Contains(t, buf.String(), "# bash completion for check")
+	}
+	{
+		buf.Reset()
+		assert.NoError(t, completionCmd.RunE(completionCmd, []string{zshFormat}))
+		assert.Contains(t, buf.String(), "#compdef check")
+	}
+}
