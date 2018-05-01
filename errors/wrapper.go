7252a307c6bd762414965dc507060fda1690db26
Kamil Samigullin <kamil@samigullin.info>
2018-05-01 18:35:05 +0300

    issue #45: improve errors package

--- errors/wrapper.go
+++ errors/wrapper.go
@@ -8,10 +8,12 @@ import (
 	"github.com/pkg/errors"
 )
 
+// Errorf is a proxy for `github.com/pkg/errors.Errorf`.
 func Errorf(format string, args ...interface{}) error {
 	return errors.Errorf(format, args...)
 }
 
+// Recover recovers execution flow and sets error to the passed error pointer.
 func Recover(err *error) {
 	if r := recover(); r != nil {
 		if e, is := r.(error); is {
@@ -22,10 +24,13 @@ func Recover(err *error) {
 	}
 }
 
+// Simple returns text-based error without stack trace.
 func Simple(message string) error {
 	return core.New(message)
 }
 
+// StackTrace tries to extract stack trace from the error
+// or returns nil if it can't.
 func StackTrace(err error) errors.StackTrace {
 	type stackTracer interface {
 		StackTrace() errors.StackTrace
@@ -37,10 +42,12 @@ func StackTrace(err error) errors.StackTrace {
 	return nil
 }
 
+// WithMessage is a proxy for `github.com/pkg/errors.WithMessage`.
 func WithMessage(err error, message string) error {
 	return errors.WithMessage(err, message)
 }
 
+// Wrapf is a proxy for `github.com/pkg/errors.Wrapf`.
 func Wrapf(err error, format string, args ...interface{}) error {
 	return errors.Wrapf(err, format, args...)
 }
